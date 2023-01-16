8e6d63eb34b5671500850a03cc854d89661b4436
Gizmosoft <Kartikey.hebbar@gmail.com>
2023-01-16 22:47:29 +0530

    React App skeleton setup completed

---
+++ premierleague-dashboard/src/frontend/src/components/MatchSmallCard.js
@@ -0,0 +1,12 @@
+import {React} from 'react';
+
+export const MatchSmallCard = () => {
+  return (
+    <div className="MatchSmallCard">
+        <h5>Match small card</h5>
+        <h5>Match small card</h5>
+        <h5>Match small card</h5>
+        <h5>Match small card</h5>
+    </div>
+  );
+}
